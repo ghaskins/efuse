baeb79853fb09ea8e32efe51e13805f05b72e261
Gregory Haskins <ghaskins@novell.com>
2011-05-24 15:49:48 -0400

    Mount now works
    Signed-off-by: Gregory Haskins <ghaskins@novell.com>

--- c_src/fuse_nif.c
+++ c_src/fuse_nif.c
@@ -2,18 +2,21 @@
 #define _FILE_OFFSET_BITS 64
 
 #include <fuse.h>
+#include <fuse_lowlevel.h>
+
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <erl_nif.h>
 
-#define error_tuple(msg) enif_make_tuple(env, enif_make_atom(env, "error"), enif_make_string(env, msg, ERL_NIF_LATIN1)) 
+#define error_tuple(msg) enif_make_tuple2(env, enif_make_atom(env, "error"), enif_make_string(env, msg, ERL_NIF_LATIN1)) 
 
 static ERL_NIF_TERM mount(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
   char path[1024];
-  long fd;
+  struct fuse_chan *ch;
+  int fd;
 
   if (argc != 1)
     return error_tuple("Bad argument count");
@@ -21,9 +24,16 @@ static ERL_NIF_TERM mount(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
   if (enif_get_string(env, argv[0], path, sizeof(path), ERL_NIF_LATIN1) <= 0)
     return error_tuple("Bad path");
 
-  fd = (long)fuse_mount(path, NULL);
+  char *defarg[2] = {"efuse", NULL};
+  struct fuse_args fargs = FUSE_ARGS_INIT(1, defarg);
 
-  return enif_make_tuple(env, enif_make_atom(env, "ok"), enif_make_long(env, fd));
+  ch = fuse_mount(path, &fargs);
+  if (!ch) {
+    return error_tuple("Could not mount filesystem");
+  }
+
+  fd = fuse_chan_fd(ch);
+  return enif_make_tuple2(env, enif_make_atom(env, "ok"), enif_make_int(env, fd));
 }
 
 static ErlNifFunc nif_funcs[] =
