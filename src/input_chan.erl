27be07c14346aad88e34a9693675d326573c3212
Gregory Haskins <ghaskins@novell.com>
2011-05-25 16:08:29 -0400

    Basic protocol decode working
    Signed-off-by: Gregory Haskins <ghaskins@novell.com>

--- src/input_chan.erl
+++ src/input_chan.erl
@@ -4,6 +4,10 @@
 -export([start_link/1, init/1]).
 -export([handle_event/3, handle_sync_event/4, handle_info/3, terminate/3, code_change/4]).
 
+-export([connecting/2]).
+
+-include("fuse.hrl").
+
 -record(state, {fd, port}).
 
 start_link(Fd) ->
@@ -11,7 +15,7 @@ start_link(Fd) ->
 
 init(Fd) ->
     Port = erlang:open_port({fd, Fd, Fd}, [stream, binary]),
-    {ok, idle, #state{fd=Fd, port=Port}}.
+    {ok, connecting, #state{fd=Fd, port=Port}}.
 
 handle_event(Event, _StateName, _State) -> 
     erlang:throw({"Bad event", Event}).
@@ -19,12 +23,29 @@ handle_event(Event, _StateName, _State) ->
 handle_sync_event(Event, _From, _StateName, _State) -> 
     erlang:throw({"Bad event", Event}).
 
-handle_info({Port, {data, Data}}, idle, State) ->
-    io:format("Info: ~p~n", [Data]),
-    {next_state, StateName, State}.
+handle_info({Port, {data, Data}}, StateName, State) ->
+    io:format("Data: ~p (~p)~n", [Data, size(Data)]),
+    ?MODULE:StateName(Data, State).
 
 terminate(_Reason, _StateName, _State) ->
     ok.
 
 code_change(_OldVsn, _StateName, State, _Extra) ->
     {ok, State}.
+
+decode_in_header(<<?IN_HEADER_SIZE:32/native,
+		   OpCode:32/native,
+		   Unique:64/native,
+		   NodeId:64/native,
+		   Uid:32/native,
+		   Gid:32/native,
+		   Pid:32/native,
+		   _T/binary>>=Data)
+  when size(Data) =:= ?IN_HEADER_SIZE ->
+    #in_header{opcode=OpCode, unique=Unique, nodeid=NodeId, uid=Uid, gid=Gid, pid=Pid}.
+
+connecting(Data, State) when is_binary(Data) ->
+    connecting(decode_in_header(Data), State);
+connecting(#in_header{opcode=?FUSE_INIT}=Header, State) ->
+    io:format("Initialized~n", []),
+    {next_state, connecting, State}.
