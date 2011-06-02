c37652023b13e9fd7358a35f9419a7e20c8448c7
Gregory Haskins <ghaskins@novell.com>
2011-06-02 12:08:13 -0400

    Use negative errno values in errors
    Signed-off-by: Gregory Haskins <ghaskins@novell.com>

--- src/decoder.erl
+++ src/decoder.erl
@@ -17,8 +17,9 @@ process(Header, Payload, State) ->
     {ok, Error, Reply} = try decode(OpCode, Header, Payload, State)
 			 catch
 			     error:undef ->
-				 error_logger:error_msg("OpCode: ~p undefined~n", [OpCode]),
-				 {ok, ?ENOSYS, []};
+				 error_logger:error_msg("OpCode: ~p undefined~n",
+							[OpCode]),
+				 {ok, -?ENOSYS, []};
 			     T:E ->
 				 error_logger:error_msg("General request failure: H:~p P:~p E: ~p:~p~n",
 							[Header, Payload, T, E]),
@@ -27,9 +28,9 @@ process(Header, Payload, State) ->
     send([#out_header{error=Error, unique=Header#in_header.unique} | Reply]).
 
 decode(?FUSE_LOOKUP, _Header, _Payload, _State) ->
-    {ok, ?ENOSYS, []};
+    {ok, -?ENOSYS, []};
 decode(?FUSE_FORGET, _Header, _Payload, _State) ->	
-    {ok, ?ENOSYS, []};
+    {ok, -?ENOSYS, []};
 decode(?FUSE_GETATTR, Header, _Payload, State) ->
     Module = State#state.handler,
     case Module:getattr(Header#in_header.nodeid, State#state.cookie) of
@@ -37,29 +38,29 @@ decode(?FUSE_GETATTR, Header, _Payload, State) ->
 	    {ok, 0, [Result]}
     end;
 decode(?FUSE_SETATTR, _Header, _Payload, _State) ->
-    {ok, ?ENOSYS, []};
+    {ok, -?ENOSYS, []};
 decode(?FUSE_READLINK, _Header, _Payload, _State) ->
-    {ok, ?ENOSYS, []};
+    {ok, -?ENOSYS, []};
 decode(?FUSE_SYMLINK, _Header, _Payload, _State) ->
-    {ok, ?ENOSYS, []};
+    {ok, -?ENOSYS, []};
 decode(?FUSE_MKNOD, _Header, _Payload, _State) -> 
-    {ok, ?ENOSYS, []};
+    {ok, -?ENOSYS, []};
 decode(?FUSE_MKDIR, _Header, _Payload, _State) -> 
-    {ok, ?ENOSYS, []};
+    {ok, -?ENOSYS, []};
 decode(?FUSE_UNLINK, _Header, _Payload, _State) ->
-    {ok, ?ENOSYS, []};
+    {ok, -?ENOSYS, []};
 decode(?FUSE_RMDIR, _Header, _Payload, _State) -> 
-    {ok, ?ENOSYS, []};
+    {ok, -?ENOSYS, []};
 decode(?FUSE_RENAME, _Header, _Payload, _State) ->
-    {ok, ?ENOSYS, []};
+    {ok, -?ENOSYS, []};
 decode(?FUSE_LINK, _Header, _Payload, _State) ->  
-    {ok, ?ENOSYS, []};
+    {ok, -?ENOSYS, []};
 decode(?FUSE_OPEN, _Header, _Payload, _State) ->  
-    {ok, ?ENOSYS, []};
+    {ok, -?ENOSYS, []};
 decode(?FUSE_READ, _Header, _Payload, _State) ->  
-    {ok, ?ENOSYS, []};
+    {ok, -?ENOSYS, []};
 decode(?FUSE_WRITE, _Header, _Payload, _State) -> 
-    {ok, ?ENOSYS, []};
+    {ok, -?ENOSYS, []};
 decode(?FUSE_STATFS, _Header, _Payload, State) ->
     Module = State#state.handler,
     try Module:statfs(State#state.cookie) of
@@ -70,49 +71,49 @@ decode(?FUSE_STATFS, _Header, _Payload, State) ->
 	    {ok, 0, [#kstatfs{namelen=255, bsize=512}]}
     end;
 decode(?FUSE_RELEASE, _Header, _Payload, _State) ->       
-    {ok, ?ENOSYS, []};
+    {ok, -?ENOSYS, []};
 decode(?FUSE_FSYNC , _Header, _Payload, _State) ->        
-    {ok, ?ENOSYS, []};
+    {ok, -?ENOSYS, []};
 decode(?FUSE_SETXATTR, _Header, _Payload, _State) ->      
-    {ok, ?ENOSYS, []};
+    {ok, -?ENOSYS, []};
 decode(?FUSE_GETXATTR, _Header, _Payload, _State) ->      
-    {ok, ?ENOSYS, []};
+    {ok, -?ENOSYS, []};
 decode(?FUSE_LISTXATTR, _Header, _Payload, _State) ->     
-    {ok, ?ENOSYS, []};
+    {ok, -?ENOSYS, []};
 decode(?FUSE_REMOVEXATTR, _Header, _Payload, _State) ->   
-    {ok, ?ENOSYS, []};
+    {ok, -?ENOSYS, []};
 decode(?FUSE_FLUSH, _Header, _Payload, _State) ->         
-    {ok, ?ENOSYS, []};
+    {ok, -?ENOSYS, []};
 decode(?FUSE_OPENDIR, _Header, _Payload, _State) ->       
-    {ok, ?ENOSYS, []};
+    {ok, -?ENOSYS, []};
 decode(?FUSE_READDIR, _Header, _Payload, _State) ->       
-    {ok, ?ENOSYS, []};
+    {ok, -?ENOSYS, []};
 decode(?FUSE_RELEASEDIR, _Header, _Payload, _State) ->    
-    {ok, ?ENOSYS, []};
+    {ok, -?ENOSYS, []};
 decode(?FUSE_FSYNCDIR, _Header, _Payload, _State) ->      
-    {ok, ?ENOSYS, []};
+    {ok, -?ENOSYS, []};
 decode(?FUSE_GETLK, _Header, _Payload, _State) ->         
-    {ok, ?ENOSYS, []};
+    {ok, -?ENOSYS, []};
 decode(?FUSE_SETLK, _Header, _Payload, _State) ->         
-    {ok, ?ENOSYS, []};
+    {ok, -?ENOSYS, []};
 decode(?FUSE_SETLKW, _Header, _Payload, _State) ->        
-    {ok, ?ENOSYS, []};
+    {ok, -?ENOSYS, []};
 decode(?FUSE_ACCESS, _Header, _Payload, _State) ->        
-    {ok, ?ENOSYS, []};
+    {ok, -?ENOSYS, []};
 decode(?FUSE_CREATE, _Header, _Payload, _State) ->        
-    {ok, ?ENOSYS, []};
+    {ok, -?ENOSYS, []};
 decode(?FUSE_INTERRUPT, _Header, _Payload, _State) ->    
-    {ok, ?ENOSYS, []};
+    {ok, -?ENOSYS, []};
 decode(?FUSE_BMAP, _Header, _Payload, _State) ->          
-    {ok, ?ENOSYS, []};
+    {ok, -?ENOSYS, []};
 decode(?FUSE_DESTROY, _Header, _Payload, _State) ->       
-    {ok, ?ENOSYS, []};
+    {ok, -?ENOSYS, []};
 decode(?FUSE_IOCTL, _Header, _Payload, _State) ->         
-    {ok, ?ENOSYS, []};
+    {ok, -?ENOSYS, []};
 decode(?FUSE_POLL, _Header, _Payload, _State) ->
-    {ok, ?ENOSYS, []};
+    {ok, -?ENOSYS, []};
 decode(?FUSE_NOTIFY_REPLY, _Header, _Payload, _State) ->  
-    {ok, ?ENOSYS, []};
+    {ok, -?ENOSYS, []};
 decode(?FUSE_BATCH_FORGET, _Header, _Payload, _State) ->  
-    {ok, ?ENOSYS, []}.
+    {ok, -?ENOSYS, []}.
 
