ef4360a871d346f8dcda31cfbb07bbf90fafcfaa
Gregory Haskins <ghaskins@novell.com>
2011-06-14 22:57:35 -0400

    Fixed dirent alignment issue
    Signed-off-by: Gregory Haskins <ghaskins@novell.com>

--- src/dirent.erl
+++ src/dirent.erl
@@ -4,7 +4,7 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
--define(DIRENT_SIZE, 25).
+-define(DIRENT_SIZE, 24).
 
 namelen(Dirent) ->
     length(Dirent#dirent.name).
@@ -12,18 +12,27 @@ namelen(Dirent) ->
 compute_size(Dirent) ->
     ?DIRENT_SIZE + namelen(Dirent).
 
+align64(X) -> (X + 7) band -8. 
+
 encode(Dirents) ->
     F = fun(#dirent{ino=Ino, name=Name, type=Type}=Dirent, Offset) ->
 		BinaryName = list_to_binary(Name),
 		Namelen = namelen(Dirent),
-		Size = ?DIRENT_SIZE + namelen(Dirent),
+		Len = ?DIRENT_SIZE + namelen(Dirent),
+		Size = align64(Len),
+		Padding = binary:list_to_bin(lists:duplicate(Size-Len, 0)),
 		NewOffset = Size+Offset,
 		EncodedDirent = <<Ino:64/native,
 				  NewOffset:64/native,
 				  Namelen:32/native,
 				  Type:32/native,
 				  BinaryName/binary,
-				  0:8>>,
+				  Padding/binary>>,
+
+		if
+		    size(EncodedDirent) =:= Size -> ok
+		end,
+
 		{EncodedDirent, NewOffset}
 	end,
     {Data, _} = lists:mapfoldl(F, 0, Dirents),
